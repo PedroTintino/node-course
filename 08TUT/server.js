071aadcfc6ea997d7905a3281d69d9c56f3f4dec
Kaworudesu <pedro.arcanjo2002@gmail.com>
2023-12-06 00:39:55 -0300

    changes

--- 08TUT/server.js
+++ 08TUT/server.js
@@ -4,36 +4,25 @@ const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500' ]
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin){
-            callback(null, true)
-        } else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
 // Serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // ROUTES
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 // Now with someone request something that doesn't exist using all (*)
